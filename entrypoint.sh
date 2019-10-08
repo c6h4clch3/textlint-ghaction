1c972cddde45c96ce9dcbeda24beb656922ec197
c6h4clch3 <c6h4clch3@gmail.com>
2019-10-09 01:02:15 +0900

    fix: 一時変数に追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ if [ $INPUT_CONFIGFILE -eq "" ]; then
   configFile="--config $INPUT_CONFIGFILE"
 fi
 
-res = $(textlint --plugin review --preset preset-ja-technical-writing \
+res=$(textlint --plugin review --preset preset-ja-technical-writing \
   ${configFile} $INPUT_WORKDIR)
 echo ::set-output name=textlint_result::$res
