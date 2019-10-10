3f88b9e9e1348dd7f0c840c35ac98ff68e89f6e8
c6h4clch3 <c6h4clch3@gmail.com>
2019-10-11 03:39:29 +0900

    fix: 返り値反映

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ if [ $INPUT_CONFIGFILE -eq "" ]; then
 fi
 
 textlint --plugin review --preset preset-ja-technical-writing \
-  ${configFile} $INPUT_WORKDIR > res.log
+  ${configFile} $INPUT_WORKDIR > res.log || EXIT_CODE=$?
+cat res.log
 echo ::set-output name=textlint_result::$(cat res.log)
+return ${EXIT_CODE}
