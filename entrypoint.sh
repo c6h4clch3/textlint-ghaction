88fb6e719b704f4aab908e0590c095ac5fe3d367
c6h4clch3 <c6h4clch3@gmail.com>
2019-10-11 12:29:56 +0900

    fix: return

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ lint() {
     if [ $INPUT_CONFIGFILE -eq "" ]; then
         configFile="--config $INPUT_CONFIGFILE"
     fi
-    return textlint --plugin review --preset preset-ja-technical-writing \
-    ${configFile} $INPUT_WORKDIR
+    return $(textlint --plugin review --preset preset-ja-technical-writing \
+    ${configFile} $INPUT_WORKDIR)
 }
 if [ $INPUT_SILENT -ne "true" ]; then
-    return lint
+    return $(lint)
 fi
 
 lint > res.log || EXIT_CODE=$?
