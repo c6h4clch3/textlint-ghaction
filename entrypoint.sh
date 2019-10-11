7c7c111bd2978773688b3264a770712e36381d88
c6h4clch3 <c6h4clch3@gmail.com>
2019-10-11 12:50:10 +0900

    fix: not eq

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 lint() {
-    if [ $INPUT_CONFIGFILE -ne "" ]; then
+    if [ $INPUT_CONFIGFILE != "" ]; then
         configFile="--config $INPUT_CONFIGFILE"
     fi
     textlint --plugin review --preset preset-ja-technical-writing ${configFile} $INPUT_WORKDIR
     return $?
 }
-if [ $INPUT_SILENT -ne "true" ]; then
+if [ $INPUT_SILENT != "true" ]; then
     lint
     exit $?
 fi
