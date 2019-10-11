9905fffc741bfdaee9e9911e5e84c257214bef69
c6h4clch3 <c6h4clch3@gmail.com>
2019-10-11 12:19:53 +0900

    add: サイレントモード

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.10
 
-RUN apk update && apk add npm
+RUN apk --update add npm
 RUN set -x && \
   npm install -g textlint@11.3.1 \
   textlint-rule-preset-ja-technical-writing@3.1.3 \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh -l
 
-if [ $INPUT_CONFIGFILE -eq "" ]; then
-  configFile="--config $INPUT_CONFIGFILE"
+lint() {
+    if [ $INPUT_CONFIGFILE -eq "" ]; then
+        configFile="--config $INPUT_CONFIGFILE"
+    fi
+    return textlint --plugin review --preset preset-ja-technical-writing \
+    ${configFile} $INPUT_WORKDIR
+}
+if ![ $INPUT_SILENT -eq "true" ]; then
+    return lint
 fi
 
-textlint --plugin review --preset preset-ja-technical-writing \
-  ${configFile} $INPUT_WORKDIR > res.log || EXIT_CODE=$?
-cat res.log
+
+lint > res.log || EXIT_CODE=$?
 echo ::set-output name=textlint_result::$(cat res.log)
 return ${EXIT_CODE}
