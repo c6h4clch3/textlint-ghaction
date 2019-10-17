cef5784bac108cd5384e307333974c47f9381b34
c6h4clch3 <c6h4clch3@gmail.com>
2019-10-17 15:25:59 +0900

    modify: remove output log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 lint() {
-    if [ $INPUT_CONFIG_FILE != "" ]; then
+    if [ "$INPUT_CONFIG_FILE" != "" ]; then
         configFile="--config $INPUT_CONFIG_FILE"
     fi
     textlint --plugin review --preset preset-ja-technical-writing ${configFile} $1 $INPUT_TARGET
     return $?
 }
 
-if [ $INPUT_USE_REVIEWDOG != "" ] && [ $GITHUB_TOKEN != "" ]; then
+if [ "$INPUT_USE_REVIEWDOG" != "" ] && [ "$GITHUB_TOKEN" != "" ]; then
     lint --format checkstyle > res.log
     cat res.log | \
     REVIEWDOG_GITHUB_API_TOKEN=$GITHUB_TOKEN \
